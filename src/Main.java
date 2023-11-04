7af4c117740fc1c48c4723a503fe8d45098abc37
Daniel Sérgio <daniel.vm26@yahoo.com>
2023-11-04 14:34:55 -0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        Iphone celular = new Iphone();
+
+        celular.ligar();
+        celular.atender();
+        celular.iniciarCorreioVoz();
+
+        celular.tocar();
+        celular.pausar();
+        celular.selecionarMusica();
+
+        celular.exibirPagina();
+        celular.adicionarNovaAba();
+        celular.atualizarPagina();
+    }
+}
